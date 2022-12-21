83ed37452ec8e0e4f8bca17e305c541bab0d167d
Matheus Borges Teixeira <matheus.bt@gmail.com>
2022-12-21 22:46:55 +0100

    Giving up on graphs.

--- 2022/day12.js
+++ 2022/day12.js
@@ -13,7 +13,8 @@ let to;
 
 const input = read('./day12.input', testInput)
     .split('\n')
-    .filter(i => i)
+    .filter(i => i);
+const data = input
     .map((i, indexi) => i.split('').map((j, indexj) => {
       switch(j) {
         case 'S':
@@ -30,10 +31,7 @@ const input = read('./day12.input', testInput)
 console.log(input, from, to);
 
 let paths = [
-  {
-    visited: [],
-    path: [from],
-  }
+  [from]
 ]
 
 let pathsFound = [];
@@ -51,33 +49,61 @@ function possibleDirections(heightmap, position) {
   })
 }
 
-const visited = [];
-let count = 0
-while((paths.length > 0) && (pathsFound.length < 1000)) {
-  const trial = paths.shift();
-  const destination = trial.path[trial.path.length - 1];
-  // console.log('trying', trial);
-  if(count++ % 100 === 0) console.log(trial.path.length, trial.visited?.length, paths.length);
-  // if(trial.length > 30) console.log(trial)
-  if(destination.toString() === to.toString()) {
-    console.log("New", trial.path.length);
-    pathsFound.push(trial);
-  } else {
-    const directions = possibleDirections(input, destination);
-    directions
-      .filter(i => !trial.path.map(i => i.toString()).includes(i.toString()))
-      // .filter(i => !trial.visited.map(i => i.toString()).includes(i.toString()))
-      .forEach(i => paths.push({
-        // visited: trial.visited.concat([destination]),
-        path: trial.path.concat([i]),
-      }));
-    paths = paths
-      .sort((a,b) => b.path.length - a.path.length)
-      // .sort((a,b) => -(input[b.path[b.path.length-1][0]][b.path[b.path.length-1][1]] - input[a.path[a.path.length-1][0]][a.path[a.path.length-1][1]]))
+function printVisited() {
+  const screen = [];
+  for(let i = 0; i < input.length; i++) {
+    screen[i] = Array.apply('.', {length: input[0].length});
+    for(let j = 0; j < input[0].length; j++) {
+      screen[i][j] = input[i][j];
+    }
   }
+  visited.forEach(point => {
+    screen[point[0]][point[1]] = screen[point[0]][point[1]].toUpperCase();
+  });
+  console.log(">>>", visited.length, paths.length)
+  console.log(screen.map(i => i.join('')).join('\n'))
 }
 
+const visited = [];
+const maxSize = input.length * input[0].length;
+let count = 0
+while((paths.length > 0) && (pathsFound.length <= 100) && (visited.length <= maxSize)) {
+  const trial = paths.shift();
+  const destination = trial[trial.length - 1];
+  // console.log(destination)
+  if(!visited.map(i => i.join(',').toString()).includes(destination.join(',').toString())) {
+    visited.push(destination);
+    printVisited()
+  }
+  if(destination.toString() === to.toString()) {
+    console.log("New", trial.length);
+    pathsFound.push(trial);
+  } else {
+    const directions = possibleDirections(data, destination)
+      .filter(i => !trial.map(i => i.join(',').toString()).includes(i.join(',').toString()))
+      // .filter(i => !visited.map(i => i.toString()).includes(i.toString()));
+    // if(directions.length <= 0) console.log("wtf")
+    directions
+      .forEach(i => {
+        // console.log(i)
+        paths.push(trial.concat([i]))
+      });
+    paths = paths
+      .filter(i => paths.map(i => JSON.stringify(i)).includes(JSON.stringify(i)))
+      // .filter(i => i.length <= maxSize)
+      // .filter(i => i.filter(i => !visited.map(i => i.join(',').toString()).includes(i.join(','))).length > 0)
+      .sort((a,b) => b.length - a.length)
+      // .sort((a,b) => (data[b[b.length-1][0]][b[b.length-1][1]] - data[a[a.length-1][0]][a[a.length-1][1]]))
+      // .sort((a,b) => {
+      //   const size = (a.length > b.length) ? 1 : -1;
+      //   const height = 0;(data[a[a.length-1][0]][a[a.length-1][1]] < data[b[b.length-1][0]][b[b.length-1][1]]) ? 1 : -1;
+      //   return size + height;
+      // })
+  }
+  // printVisited();
+}
+console.log((paths.length > 0), (pathsFound.length <= 100), (visited.length < maxSize))
 pathsFound = pathsFound.sort((a,b) => a.length - b.length)
-console.log(pathsFound.length, pathsFound[0].length -1, paths.length)
+console.log("end", pathsFound.length, pathsFound[0]?.length - 1, paths.length, maxSize)
 // 1438 too high
 // 1418 too high
